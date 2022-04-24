9a4745fb85520f81453be305be3049a7fc92ad7a
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2022-04-24 00:44:06 +0000

    deploy: dc6b1647ac6bce593159e8d24ba751eeb26480b0

---
+++ implementors/oo7/dbus/trait.Unlockable.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["oo7"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
